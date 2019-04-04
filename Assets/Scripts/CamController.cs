7cfc788e42b75d21270fed36cc093929f3d96636
cford35 <cford35@gatech.edu>
2019-04-04 13:14:56 -0400

    Added movement in the overworldx

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+	public GameObject target;
+	private Vector3 targetPos;
+	public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
